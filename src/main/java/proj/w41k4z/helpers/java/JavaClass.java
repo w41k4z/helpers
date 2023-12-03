21feac3d16a3e667d660a36329244fe24687dc0f
w41k4z <alainricor@gmail.com>
2023-12-03 16:38:11 +0300

    Unused import removed

--- src/main/java/proj/w41k4z/helpers/java/JavaClass.java
+++ src/main/java/proj/w41k4z/helpers/java/JavaClass.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
